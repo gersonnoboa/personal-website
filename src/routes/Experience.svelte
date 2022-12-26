128ad5e5b1dec8742f797784e8cec9f9caf94e4d
Gerson Noboa <gerson.noboa@gmail.com>
2022-12-26 18:32:11 +0200

    Change hero and experience descriptions

--- src/routes/Experience.svelte
+++ src/routes/Experience.svelte
@@ -9,11 +9,10 @@
 		period="July 2020 - Present"
 		description="Work on the mobile SDKs that are integrated into our customers' apps, in order to create rich,
 	native digital customer service experiences."
-		accomplishments="During my time at the company, the native SDK has been released and integrated by multiple
-	companies and partners. I have also led the implementation of push notifications for both platforms
-	and massive improvements to our CI/CD workflow in Bitrise. I have interviewed multiple mobile
-	candidates and have been directly responsible in the hiring of four internal developers and
-	two contractors."
+		accomplishments="During my time here, the native SDK has been released and integrated by multiple
+	companies. I led the implementation of push notifications for both platforms and massive improvements
+	to our CI/CD workflow in Bitrise. I interviewed multiple mobile candidates and have been directly responsible
+	in the hiring of several developers."
 		website="https://www.glia.com"
 	/>
 
@@ -31,12 +30,10 @@
 	<ExperienceCard
 		title="Proekspert · Software Engineer"
 		period="April 2018 - December 2019"
-		description="Worked on the native, customer-facing, self-service iOS application for Telia Finland.
-		The application is used by around 100k customers monthly."
-		accomplishments="I led the deployment of Clean Swift for the project, to improve the testability, scalability
-		and developer experience. I was directly responsible for the implementation of translations
-		into English and Swedish, after compiling all concerns raised by customers on our app reviews and
-		presenting them to key stakeholders. I was part of the implementation of various new features,
+		description="Worked on the self-service iOS application for Telia Finland, which boasts around 100k monthly users."
+		accomplishments="I led the deployment of Clean Swift for the project. I was directly responsible for
+		the implementation of translations, after compiling concerns raised by customers and
+		presenting them to key stakeholders. I was part of the implementation of features
 		like store locator, booking appointments through the app, and a complete visual revamp."
 		website="https://www.proekspert.com"
 	/>
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,11 +1,11 @@
-<div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">
-	<div class="hero-overlay bg-opacity-90" />
-	<div class="hero-content text-center text-neutral-content">
+<div class="hero min-h-screen bg-gradient-to-r from-primary to-secondary">
+	<div class="hero-overlay bg-opacity-50" />
+	<div class="hero-content text-center text-primary-content">
 		<div class="max-w-md">
 			<h1 class="mb-5 text-5xl font-bold">Hello! I'm Gerson Noboa.</h1>
 			<p class="mb-5">
 				Software engineer with years of experience across multiple platforms and tech stacks. Mainly
-				proficient in iOS development with Objective-C, Swift, and all related accompanying tools.
+				proficient in iOS development with Swift, Objective-C, and all related accompanying tools.
 				Currently working as a mobile engineer for Glia, primarily on our iOS offerings.
 			</p>
 			<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
